e9e1503e1fae895f3b0cec431d5ff606045c9f6a
Oboo Chin <cheng.12@live.cn>
2017-02-24 23:46:25 +0800

    Update mockup.

---
+++ mockup/index.js
@@ -0,0 +1,20 @@
+import React,{ Components } from 'react';
+import {
+	Text
+} from 'react-native'
+
+import RNPatternLock from 'react-native-pattern-lock'
+
+export default class MainScreen extends Components{
+	redner(){
+		return (
+			<View>
+				<RNPatternLock
+					onStart = { this.onStart }
+					onEnd = { this.onEnd }
+					status = 'setting/lock'
+				/>
+			</View>
+		)
+	}
+}
